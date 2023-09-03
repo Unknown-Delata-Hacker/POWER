409c6a3b08a52cf37d7e15cfea1edd3b90382387
Unknown-Delata-Hacker <143934334+Unknown-Delata-Hacker@users.noreply.github.com>
2023-09-03 17:03:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,7 @@
-import subprocess
-
-
-def run_command():
-    # Install cmake
-    try:
-        subprocess.run(["pip", "install", "cmake"])
-    except subprocess.CalledProcessError as e:
-        print(f"Error installing cmake: {e}")
-
-    # Install dlib
-    try:
-        subprocess.run(["pip", "install", "dlib"])
-    except subprocess.CalledProcessError as e:
-        print(f"Error installing dlib: {e}")
-
-    # Install face_recognition
-    try:
-        subprocess.run(["pip", "install", "face_recognition"])
-    except subprocess.CalledProcessError as e:
-        print(f"Error installing face_recognition: {e}")
-
-    # Verify the installation
-    try:
-        subprocess.run(["python", "-c", "import face_recognition; print('face_recognition library is installed and ready to use.')"])
-    except subprocess.CalledProcessError as e:
-        print(f"Error verifying installation: {e}")
-
+import face_recognition
+import streamlit as st
 
 def main():
-    import face_recognition
-    import streamlit as st
-
     st.write(
         """
     # My first app
@@ -40,5 +11,4 @@ def main():
 
 
 if __name__ == "__main__":
-    run_command()
     main()
