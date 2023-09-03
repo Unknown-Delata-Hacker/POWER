93e1607dd2107a4cbbd44d5660a0d8b6c266491a
Unknown-Delata-Hacker <143934334+Unknown-Delata-Hacker@users.noreply.github.com>
2023-09-03 15:49:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ import subprocess
 
 
 def run_command():
+    # Install dlib
     try:
-        subprocess.run(["xcode-select", "--install"], check=True)
-        print("Command executed successfully")
+        subprocess.run(["pip", "install", "dlib"])
     except subprocess.CalledProcessError as e:
-        print(f"Error executing command: {e}")
+        print(f"Error installing dlib: {e}")
+
+    # Install face_recognition
+    try:
+        subprocess.run(["pip", "install", "face_recognition"])
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing face_recognition: {e}")
+
+    # Verify the installation
+    try:
+        subprocess.run(["python", "-c", "import face_recognition; print('face_recognition library is installed and ready to use.')"])
+    except subprocess.CalledProcessError as e:
+        print(f"Error verifying installation: {e}")
 
 
 def main():
